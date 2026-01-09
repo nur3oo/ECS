6b338a7ec3d7959cd706e9ad07066fa0add331e7
nur3oo <dnurdaud@gmail.com>
2026-01-09 20:44:01 +0000

    fixed provider

--- Infra/modules/acm/version.tf
+++ Infra/modules/acm/version.tf
@@ -4,8 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.62.0"
+      version = ">= 5.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 4.0"
     }
   }
 }
-
