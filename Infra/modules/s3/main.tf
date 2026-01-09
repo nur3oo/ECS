d1aea8d62410190452167cc1b5b52d843f3abe54
nur3oo <dnurdaud@gmail.com>
2026-01-09 21:57:31 +0000

    added in yaml

--- Infra/modules/s3/main.tf
+++ Infra/modules/s3/main.tf
@@ -59,7 +59,7 @@ resource "aws_cloudfront_distribution" "wiki_images_cdn" {
     custom_origin_config {
       http_port              = 80
       https_port             = 443
-      origin_protocol_policy = "https-only"
+      origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
