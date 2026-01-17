208b4299ab771922e5d84a9778d8653d57ce5e6c
nur3oo <dnurdaud@gmail.com>
2026-01-17 17:23:39 +0000

    fixed var as 16.3 isn't in my region

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -59,7 +59,7 @@ resource "random_password" "db_password" {
 }
 
 resource "aws_secretsmanager_secret" "db" {
-  name = "${var.name}/db.v1"
+  name = "${var.name}/db.v2"
 }
 
 resource "aws_secretsmanager_secret_version" "db" {
