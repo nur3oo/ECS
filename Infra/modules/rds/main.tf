c5f4bae2e0a6b7844c52e695f54807bf779d6a1e
nur3oo <dnurdaud@gmail.com>
2026-01-19 16:12:10 +0000

    fmt

--- Infra/modules/rds/main.tf
+++ Infra/modules/rds/main.tf
@@ -59,7 +59,7 @@ resource "random_password" "db_password" {
 }
 
 resource "aws_secretsmanager_secret" "db" {
-  name = "${var.name}/db.v2"
+  name = "${var.name}/db.v3"
 }
 
 resource "aws_secretsmanager_secret_version" "db" {
