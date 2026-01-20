e9afbea9dcff1906fa1af1c59c39f93d19b1359a
nur3oo <dnurdaud@gmail.com>
2026-01-20 21:05:01 +0000

    validation records were using older cloudflare names

--- Infra/modules/cdn/main.tf
+++ Infra/modules/cdn/main.tf
@@ -13,7 +13,7 @@ resource "aws_acm_certificate" "cdn" {
 resource "cloudflare_dns_record" "cdn_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cdn.domain_validation_options :
-    dvo.domain_name => {
+    dvo.resource_record_name => {
       name  = dvo.resource_record_name
       type  = dvo.resource_record_type
       value = dvo.resource_record_value
