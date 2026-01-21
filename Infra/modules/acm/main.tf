d9da797bb3bd9a44a9aa60f9741749925bf684dd
nur3oo <dnurdaud@gmail.com>
2026-01-21 17:09:14 +0000

    validation records were using older cloudflare names

--- Infra/modules/acm/main.tf
+++ Infra/modules/acm/main.tf
@@ -14,24 +14,17 @@ resource "aws_acm_certificate" "cert" {
 }
 
 
-resource "cloudflare_dns_record" "acm_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options :
-    dvo.resource_record_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
 
+
+resource "cloudflare_dns_record" "acm_validation" {
   zone_id = var.cloudflare_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  content = each.value.value
-  ttl     = 60
+  name = var.domain_name
+  ttl = 60
+  type = "CNAMEs"
   proxied = false
 }
 
+  
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
--- Infra/modules/cdn/main.tf
+++ Infra/modules/cdn/main.tf
@@ -10,21 +10,13 @@ resource "aws_acm_certificate" "cdn" {
   }
 }
 
-resource "cloudflare_dns_record" "cdn_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cdn.domain_validation_options :
-    dvo.resource_record_name => {
-      name  = dvo.resource_record_name
-      type  = dvo.resource_record_type
-      value = dvo.resource_record_value
-    }
-  }
 
+
+ resource "cloudflare_dns_record" "cdn_validation" {
   zone_id = var.cloudflare_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  content = each.value.value
-  ttl     = 60
+  name = var.domain_name
+  ttl = 60
+  type = "CNAME"
   proxied = false
 }
 
