0eb03e997d518acfb6c57050e751a81c8a480cc0
nur3oo <dnurdaud@gmail.com>
2026-01-21 17:25:13 +0000

    typo with the DNS type

--- Infra/modules/cdn/main.tf
+++ Infra/modules/cdn/main.tf
@@ -24,7 +24,7 @@ resource "cloudflare_dns_record" "cdn_validation" {
 resource "aws_acm_certificate_validation" "cd_v" {
   provider                = aws.use1
   certificate_arn         = aws_acm_certificate.cdn.arn
-  validation_record_fqdns = [cloudflare_dns_record.acm_validation.name]
+  validation_record_fqdns = [cloudflare_dns_record.cdn_validation.name]
 }
 
 
